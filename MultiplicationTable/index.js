366e302da2349616ec8fae77669e36960fa0d5d8
sakshiigothwal <119726876+sakshiigothwal@users.noreply.github.com>
2025-06-02 16:51:55 +0530

    Update index.js

--- MultiplicationTable/index.js
+++ MultiplicationTable/index.js
@@ -1,17 +1,26 @@
 let btn = document.getElementById('btn');
-btn.addEventListener("click", tableGenerator);
+let numberInput = document.getElementById('number');
+let clearAll=document.getElementById("clear");
+let ul = document.getElementById('ul');
+
 function tableGenerator(){
-    let numberInput = document.getElementById('number');
+    
     let num = numberInput.value;
+
     let i = 1;
     let j = 0;
     while(i<=10 && j<10){
         let table = num * i;
         i++;
         j++;
-        let ul = document.getElementById('ul');
         let list = document.createElement('li');
         list.textContent = num + " * " + j + " = " + table;
         ul.appendChild(list);
     }
 }
+function clearField(){
+    numberInput.value= "";
+    ul.innerHTML="";
+}
+btn.addEventListener("click", tableGenerator);
+clearAll.addEventListener("click", clearField);
